687a8ac6b7a428cc729e344401c0a8f2f99bc2ef
Natiq Javid <natiqjavid1514@gmail.com>
2026-01-02 19:36:39 +0530

    First Project of CPP

---
+++ CPP/Project1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int favorite_number;
+    std::cout<<"What is your favorite number:";
+    std::cin>>favorite_number;
+    std::cout<<"Amazing! That's my favorite number too" <<std::endl;
+
+    return 0;
+
+
+}
