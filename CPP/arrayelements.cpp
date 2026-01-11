90b16a4d58b7f3fbaffd9ecadd7bf112d75a780d
Natiq Javid <natiqjavid1514@gmail.com>
2026-01-11 10:19:15 +0530

    Aaj

---
+++ CPP/arrayelements.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // char vowels [] {'a','e','i','o','u'};
+
+    // cout<<"First vowel is:"<<vowels[0]<<endl;
+    
+    // cout<<" Last vowel is:"<<vowels[4]<<endl;
+
+
+
+    // cin>>vowels[5]; //dont do this- out of bonds
+
+    // return 0;
+
+
+    // float high_temp[] {92.5, 96.5,92.1,99.0,95.2};
+
+    // cout<<"Highest temp is"<<high_temp[3]<<endl;
+
+    // high_temp[3]=100;
+
+    // cout<<"Now the highest temp is:"<<high_temp[3]<<endl;
+    
+    // return 0; /* this is the assignment example*/
+
+
+    /* What if I dont initialize the array*/
+
+    int test_scores[] {10,20,30,40,50};
+
+    // cout<<test_scores[0]<<endl;
+
+    // cin>>test_scores[0];
+
+    // cout<<test_scores[0]<<endl;
+
+
+    cout<<test_scores<<endl;
+
+}
---
+++ CPP/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+ // char vowels [] {'a','e','i','o','u'};
+
+    // cout<<"First vowel is:"<<vowels[0]<<endl;
+    
+    // cout<<" Last vowel is:"<<vowels[4]<<endl;
+
+
+
+    // cin>>vowels[5]; //dont do this- out of bonds
+
+    // return 0;
