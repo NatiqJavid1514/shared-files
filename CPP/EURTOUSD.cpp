e01d19965c1f47c6029844bb0252fba75d8e5483
Natiq Javid <natiqjavid1514@gmail.com>
2026-01-16 16:28:44 +0530

    woooo

---
+++ CPP/EURTOUSD.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+     const double usdtoeuro{0.86};
+
+     double amounttoconvert{0};
+
+    cout<<" Enter the amount you want to convert from USD to EURO: "<<endl;
+    
+    cin>>amounttoconvert;
+
+
+    cout<<"your converted amount is:"<<amounttoconvert*usdtoeuro<<" euros"<<endl;
+ 
+
+
+
+    
+
+    
+
+
+
+
+}
