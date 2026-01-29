47b81ac41b9546f35ecd092592e15d711193a592
Natiq Javid <natiqjavid1514@gmail.com>
2026-01-29 16:12:39 +0530

    Your descriptive commit message

---
+++ CPP/ternary.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+    // int num{};
+
+    // cout<<"Enter an Integer: "<<endl;
+    // cin>>num;
+
+    // if(num%2==0){
+    //     cout<<"Even number"<<endl;
+
+
+
+
+    // }
+    // else{
+    //     cout<<"Odd number"<<endl;
+
+    // };
+
+    // cout<<num<<" is "<<((num%2==0)?"Even":"Odd");
+
+    int num1{},num2{};
+
+    cout<<"Enter your integers with spaces"<<endl;
+
+    cin>>num1>>num2;
+
+    cout<<"Larger number is "<<((num1>num2)?num1:num2);
+
+
+
+
+}
