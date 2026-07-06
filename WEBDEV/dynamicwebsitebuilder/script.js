d1842f9719c97ff3ad9ad52141bafa1a092f92cf
Natiq Javid <natiqjavid1514@gmail.com>
2026-07-06 23:30:15 +0530

    Your descriptive commit message

---
+++ WEBDEV/dynamicwebsitebuilder/script.js
@@ -0,0 +1,21 @@
+    console.log("Hello world");
+    function createcard(img,title,views,duration){
+      let html=<div class="container">
+        <div class="card">
+            <div class="image"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpUbBU6DNPkK7OnNqAQ4UFCkT4Anj3WfLAqTYXvvUHaMiPx0NKJ4mvhsk&s=10" alt=""></div>
+            <div class="title">
+                Heading, Paragraphs and Links | Sigma Web Development Course - Tutorial #4
+                <div class="meta">  <div class="views">1.5m views</div> <div style="margin-left: 20px;" class="time">• 2 years old</div></div>
+            </div>
+            
+            
+
+
+        </div>
+        
+
+
+
+    </div>
+}
+    
