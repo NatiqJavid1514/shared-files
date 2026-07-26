4b19cc1fc6590d017919735b724796538058cc91
Natiq Javid <natiqjavid1514@gmail.com>
2026-07-26 22:20:32 +0530

    Your descriptive commit message

---
+++ LogicBuildingQuestions/Arrays/sec1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    // int size;
+    // cout<<"What is the size of array"<<endl;
+    // cin>>size;
+
+    // int arr[size];
+    // cout<<"enter array elements: "<<endl;
+    // for(int i=0;i<size;i++){
+    //     cin>>arr[i];
+    // }
+    // cout<<"The array elements are: "<<endl;
+    // for(int i=0;i<size;i++){
+    //     cout<<arr[i]<<endl;
+        
+    // }
+  
+    //find sum of all elements in array
+
+    // int arr[5]={10,20,30,40,50};
+    // int sum=0;
+    // for(auto v:arr){
+    //     sum+=v;
+    // }
+    // cout<<sum<<endl;
+
+    // maximum minimum of array elements
+    // int arr[5]={10,20,30,40,50};
+    // int max=arr[0];
+    // //maximum
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]>max)
+    //     max=arr[i];
+    // }
+    // cout<<max<<endl;
+    // int min=arr[1];
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]<min){
+    //         min=arr[i];
+    //     }
+    // }
+    // cout<<min<<endl;
+        
+
+
+
+}
---
+++ LogicBuildingQuestions/Arrays/sec2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    //search an element in array
+    // int arr[5]={10,20,30,40,50};
+    // int n;
+    // cout<<"enter number to find in array"<<endl;
+    // cin>>n;
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]==n)
+    //     cout<<arr[i]<<endl;
+
+    // }
+    // int n=30;
+    // int arr[5]={10,20,30,40,50};
+    // for(int i=0;i<5;i++){
+    //     if(arr[i]==n){
+    //         cout<<"First occurance of "<<n<<" is at index "<<i<<endl;
+    //     }
+    // } //FOR LAST OCCURANCE TRAVERSE ARRAY FROM RIGHT TO LEFT//
+    //find if all elements of array are unique or not
+    // int arr[6]={1,2,2,4,5,6};
+    // int flag=0;
+    // for(int i=0;i<6;i++){
+    //     for(int j=(i+1);j<6;j++){
+    //         if(arr[i]==arr[j]){
+    //             flag=1;
+                
+    //         }
+
+    //     }
+
+    // }
+    // if(flag==1){
+    //     cout<<"array elements are not unique"<<endl;
+
+    // }
+    // else{
+    //     cout<<"array elements are unique"<<endl;
+    // }
+
+
+
+    //find the count of prime numbers;
+//     int count=0;
+    
+//   int arr[5]={25,9,2,6,8};
+//   for(int i=0;i<5;i++){
+//     for(int j=2;j<(arr[i]/2);j++){
+//         if(j*j==arr[i]){
+//             count++;
+//         }
+//     }
+
+    
+
+
+//   }
+//   cout<<"Number of perfect square numbers are: "<<count<<endl;
+
+
+    
+    
+
+}
