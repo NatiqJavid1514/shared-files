fa1da1517e07ce0ae8041d62370f53c8e01f81aa
Natiq Javid <natiqjavid1514@gmail.com>
2026-07-27 23:09:06 +0530

    Your descriptive commit message

---
+++ LogicBuildingQuestions/Arrays/sec3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    //new array consisting of square of elements of previous array//
+    // int arr1[5]={10,20,30,40,50};
+    // int arr2[5];
+    // for(int i=0;i<5;i++){
+    //     arr2[i]=(arr1[i]*arr1[i]);
+    // }
+
+    // for(auto vec:arr2){
+    //     cout<<vec<<endl;
+    // }
+    //  swap first and last element of array
+//    int size=5;
+//    int arr[size]={10,20,30,40,50};
+//    for(int i=0;i<size;i++){
+//     int temp;
+//     temp=arr[0];
+//     arr[0]=arr[(size-1)];
+//     arr[(size-1)]=temp;
+      
+      
+//    }
+//    cout<<arr[0]<<endl;
+//    cout<<arr[size-1]<<endl;
+
+//
+
+
+
+
+}
